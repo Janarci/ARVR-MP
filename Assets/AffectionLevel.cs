0507e5f2254f3a64815bae17790d8dd82d1f08d7
Nathan-Macabanti <73865062+Nathan-Macabanti@users.noreply.github.com>
2022-06-09 14:36:47 +0800

    Initial Affection Level

---
+++ Assets/AffectionLevel.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AffectionLevel : MonoBehaviour
+{
+    [SerializeField] private int curLevel = 1;
+    [SerializeField] private int maxLevel = 5;
+    [SerializeField] private float curAffection;
+    [SerializeField] private AnimationCurve maxAffectionPerLevel;
+
+    [Header("UI")]
+    [SerializeField] private Image bar;
+    [SerializeField] private TextMeshProUGUI levelText;
+
+    [HideInInspector]
+    [Range(0.0f, 1.0f)] private float fillPercent; // curAffection / maxAffectionPerLevel
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region Initializing UI
+        if (TryGetComponent<Image>(out Image img) && bar != null)
+        {
+            bar = img;
+        }
+        if(TryGetComponent<TextMeshProUGUI>(out TextMeshProUGUI txt) && 
+            levelText != null)
+        {
+            levelText = txt;
+        }
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bar.fillAmount = fillPercent;
+        levelText.text = curLevel.ToString();
+    }
+
+    #region Affection Level Up Functions
+    public void AddAffection(float additional)
+    {
+        if(curLevel == maxLevel)
+        {
+            Debug.Log("");
+            return;
+        }
+        curAffection += additional;
+        fillPercent = curAffection / maxAffectionPerLevel.Evaluate(curLevel);
+        if (fillPercent >= 1.0f)
+        {
+            LevelUp();
+        }
+        bar.fillAmount = fillPercent;
+    }
+
+    void LevelUp()
+    {
+        curLevel += 1;
+        curAffection = 0.0f;
+        fillPercent = curAffection / maxAffectionPerLevel.Evaluate(curLevel);
+    }
+    #endregion
+}
--- Assets/testpushscript.cs
+++ Assets/testpushscript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class testpushscript : MonoBehaviour
 {
+    public int test;
     // Start is called before the first frame update
     void Start()
     {
