032e8a4014124d0f9eefb91d3894865cf270ce11
Khalil Jan Arcilla <khalil_jan_arcilla@dlsu.edu.ph>
2022-06-20 09:07:15 +0800

    Squashed commit of the following:
    commit afbf78928fee87144fc3b63c68da4aa083017380
    Author: Khalil Jan Arcilla <khalil_jan_arcilla@dlsu.edu.ph>
    Date:   Sun Jun 19 23:34:22 2022 +0800
    
        manifests
    
    commit c9e9bef230c8d7f1270ede403d6c2e64b5c78269
    Author: Khalil Jan Arcilla <khalil_jan_arcilla@dlsu.edu.ph>
    Date:   Sun Jun 19 23:21:43 2022 +0800
    
        manifests
    
    commit c7ec7a98a6e333786fef1195dee4f3f14eaa324d
    Author: IncogLad <vincenthe5th@yahoo.com>
    Date:   Sun Jun 19 23:02:20 2022 +0800
    
        aaa 2
    
    commit 27e96af7b810d44ea357a064879556e5501abebf
    Author: IncogLad <vincenthe5th@yahoo.com>
    Date:   Sun Jun 19 22:51:01 2022 +0800
    
        aaaaaaaaaaa
    
    commit 7c74ca9b93c1d51c8e0b895fdd21768246b6d7ca
    Merge: c9c6fa4 2cde9b8
    Author: IncogLad <vincenthe5th@yahoo.com>
    Date:   Sun Jun 19 22:30:02 2022 +0800
    
        Merge branch 'main' into Vincent-UI
    
    commit c9c6fa45b4d378052b455eaaa3728376f5331c22
    Author: IncogLad <vincenthe5th@yahoo.com>
    Date:   Sun Jun 19 22:29:45 2022 +0800
    
        prefabs
    
    commit 2cde9b870550217cdfd5dd1f2fa4fd0c6e387264
    Author: Khalil Jan Arcilla <khalil_jan_arcilla@dlsu.edu.ph>
    Date:   Sun Jun 19 22:28:23 2022 +0800
    
        anim controller
    
    commit 52e03d2e4d2a8c29d2abb42a041c1936083e8e81
    Author: IncogLad <vincenthe5th@yahoo.com>
    Date:   Sun Jun 19 22:23:53 2022 +0800
    
        fixed ui progression
    
    commit 15b337d26c49d6226ec69e27364a6a339d883186
    Author: IncogLad <vincenthe5th@yahoo.com>
    Date:   Sun Jun 19 14:44:38 2022 +0800
    
        ui stuff un complete

---
+++ Assets/Script/DateScenario.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateScenario
+{
+    public int dateScenarioNumber;
+    public Dialogue[] dateDialogues;
+    public int dateDialogueCount;
+}
---
+++ Assets/Script/Dialogue.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue 
+{
+    public enum character { PLAYER = 0, GIRL = 1, NONE = 2};
+
+    public string statement;
+    public character who;
+    public bool isNextDecision;
+    public string[] decisions;
+
+    
+}
